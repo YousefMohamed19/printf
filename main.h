944929679f4e14a0d9054814911f51e2edbc64aa
yousef <yousef01121977646@gmail.com>
2023-09-18 07:14:14 -0700

    update main.h

--- main.h
+++ main.h
@@ -4,4 +4,9 @@
 int _printf(const char *format, ...);
 int _putchar(char c);
 
+/*0-task function */
+int print_char(va_list ap, params_t *params);
+int print_string(va_list ap, params_t *params);
+int print_percent(va_list ap, params_t *params);
+
 #endif /* MAIN_H */
